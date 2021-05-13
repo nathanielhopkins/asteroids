1e07a828142ddb6aa7fdb7674e8934f50871c5cc
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-05-13 15:04:52 -0700

    add event listener to index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 let MovingObject = require("./moving_object.js");
 window.MovingObject = MovingObject;
+
+document.addEventListener("DOMContentLoaded", function () {
+  let canvasEl = document.getElementById("game-canvas");
+  let ctx = canvasEl.getContext('2d');
+});
