78021578a3d092eecfee3fd373c195efa903961c
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-05-14 13:33:54 -0700

    write logic for creating a new asteroid that inherits from MovingObject and has default values for everything but position, as well as random vector for velocity

---
+++ src/asteroid.js
@@ -0,0 +1,22 @@
+const Util = require("./util");
+const MovingObject = require("./moving_object");
+
+let DEFAULTS = {
+  COLOR: 'gray',
+  RADIUS: 20,
+  SPEED: 4
+}
+
+function Asteroid(pos) {
+  let options = {};
+  options.pos = pos;
+  options.vel = Util.randomVec(DEFAULTS.SPEED);
+  options.radius = DEFAULTS.RADIUS;
+  options.color = DEFAULTS.COLOR;
+
+  MovingObject.call(this, options)
+}
+
+Util.inherits(Asteroid, MovingObject);
+
+module.exports = Asteroid;
