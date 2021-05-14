8c4cc0403417434cf347fd110aa2c39e76c5b8cc
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-05-14 13:22:58 -0700

    refactor MovingObject code as a function instead of class

--- src/moving_object.js
+++ src/moving_object.js
@@ -1,29 +1,27 @@
-class MovingObject {
-  constructor(options) {
-    this.pos = options.pos;
-    this.vel = options.vel;
-    this.radius = options.radius;
-    this.color = options.color;
-  }
+function MovingObject(options) {
+  this.pos = options.pos;
+  this.vel = options.vel;
+  this.radius = options.radius;
+  this.color = options.color;
+}
 
-  draw(ctx) {
-    ctx.fillStyle = this.color;
-    ctx.beginPath();
-    ctx.arc(
-      this.pos[0],
-      this.pos[1],
-      this.radius,
-      0,
-      2 * Math.PI,
-      true
-    );
-    ctx.fill();
-  }
+MovingObject.prototype.draw = function(ctx) {
+  ctx.fillStyle = this.color;
+  ctx.beginPath();
+  ctx.arc(
+    this.pos[0],
+    this.pos[1],
+    this.radius,
+    0,
+    2 * Math.PI,
+    true
+  );
+  ctx.fill();
+}
 
-  move() {
-    this.pos[0] = this.pos[0] + this.vel[0];
-    this.pos[1] = this.pos[1] + this.vel[1];
-  }
+MovingObject.prototype.move = function() {
+  this.pos[0] = this.pos[0] + this.vel[0];
+  this.pos[1] = this.pos[1] + this.vel[1];
 }
 
 module.exports = MovingObject;
