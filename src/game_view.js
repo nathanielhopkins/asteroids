b50a24af690cead4e815e60632be506ce0c09745
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-05-17 14:45:03 -0700

    write new class GameView in src/game_view.js; write constructor for GameView that takes in game and ctx; write GameView.prototype.start that setInterval moving game objects and rendering every 20 ms; export GameView

---
+++ src/game_view.js
@@ -0,0 +1,15 @@
+let Game = require("./game");
+
+function GameView (game, ctx) {
+  this.game = game;
+  this.ctx = ctx;
+};
+
+GameView.prototype.start = function () {
+  setInterval(() => {
+    this.game.moveObjects();
+    this.game.draw(this.ctx);
+  }, 20);
+};
+
+module.exports = GameView;
