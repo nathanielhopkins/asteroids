e82857d08f230fd12d045f93bffcddc7f9b61e21
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-05-18 12:32:57 -0700

    write MovingObject.prototype.collideWith that removes both objects from Game

--- src/moving_object.js
+++ src/moving_object.js
@@ -25,4 +25,9 @@ MovingObject.prototype.isCollidedWith = function (otherObject) {
   return distance < sumRad;
 }
 
+MovingObject.prototype.collideWith = function (otherObject) {
+  this.game.remove(otherObject);
+  this.game.remove(this);
+}
+
 module.exports = MovingObject;
