9fbc51dcf570c6faeb11827595a69360b6843d35
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-05-19 11:14:36 -0700

    write Ship class that takes in a pos and game and inherits from MovingObject and has default radius and color; also initialize vel as 0; export Ship

---
+++ src/ship.js
@@ -0,0 +1,22 @@
+const Util = require("./util");
+const MovingObject = require("./moving_object");
+
+let DEFAULTS = {
+  COLOR: 'lightgreen',
+  RADIUS: 10,
+}
+
+function Ship(pos, game){
+  let options = {};
+  options.pos = pos;
+  options.game = game;
+  options.vel = 0;
+  options.radius = DEFAULTS.RADIUS;
+  options.color = DEFAULTS.COLOR;
+
+  MovingObject.call(this, options)
+}
+
+Util.inherits(Ship, MovingObject);
+
+module.exports = Ship;
