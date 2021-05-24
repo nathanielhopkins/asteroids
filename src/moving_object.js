22299099ca81ec889176869455b782fb09711517
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-05-24 12:02:29 -0700

    modify MovingObject.prototype.move to take in timeDelta; defualt constant delta to 1 if timeDelta not provided; multiply vel *delta when modifying pos

--- src/moving_object.js
+++ src/moving_object.js
@@ -13,9 +13,10 @@ MovingObject.prototype.draw = function(ctx) {
   ctx.fill();
 }
 
-MovingObject.prototype.move = function() {
-  this.pos[0] += this.vel[0];
-  this.pos[1] += this.vel[1];
+MovingObject.prototype.move = function(timeDelta)) {
+  let delta = timeDelta || 1;
+  this.pos[0] += this.vel[0] * delta;
+  this.pos[1] += this.vel[1] * delta;
   if(this.game.isOutOfBounds(this.pos)) {
     if(this.isWrappable === true) {
       this.pos = this.game.wrap(this.pos);
